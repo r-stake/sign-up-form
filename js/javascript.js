f4be8e0102ef5d5dd238c8ae53a2c9430ac5f548
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-09-01 14:03:24 +0300

    Create function for validating missing input field values

--- js/javascript.js
+++ js/javascript.js
@@ -1,48 +1,92 @@
+let submitted = false;
+
 const form = document.querySelector("form");
+
 const firstName = document.getElementById("first-name");
 const firstNameError = document.querySelector("#first-name + span.error-message");
+const firstNameLabel = document.querySelector(".first-name > label");
+
 const lastName = document.getElementById("last-name");
 const lastNameError = document.querySelector("#last-name + span.error-message");
-let submitted = false;
+const lastNameLabel = document.querySelector(".last-name > label")
+
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error-message");
+const emailLabel = document.querySelector(".email > label");
+
+const phone = document.getElementById("phone-number");
+const phoneError = document.querySelector("#phone-number + span.error-message");
+const phoneLabel = document.querySelector(".phone > label");
+
+const password = document.getElementById("password");
+const passwordError = document.querySelector(".password1 > span");
+const passwordLabel = document.querySelector(".password1 > label");
+
+const password2 = document.getElementById("confirm-password");
+const password2Error = document.querySelector(".password2 > span");
+const passwordLabel2 = document.querySelector(".password2 > label");
+
 
 form.addEventListener("submit", (event) => {
     submitted = true;
-    if (!firstName.value) {
-        firstNameError.innerHTML = `<i class="bi bi-patch-exclamation"></i> First name is required`;
-        firstName.classList.add("active-error");
-        event.preventDefault();
-    } else {
-        firstNameError.textContent = "";
-        firstName.classList.remove("active-error");
-    }
-
-    if (!lastName.value) {
-        lastNameError.innerHTML = `<i class="bi bi-patch-exclamation"></i> Last name is required`;
-        lastName.classList.add("active-error");
-        event.preventDefault();
-    }
+    validateForMissingValue(firstName, firstNameError, firstNameLabel, event);
+    validateForMissingValue(lastName, lastNameError, lastNameLabel, event);
+    validateForMissingValue(email, emailError, emailLabel, event);
+    validateForMissingValue(phone, phoneError, phoneLabel, event);
+    validateForMissingValue(password, passwordError, passwordLabel, event);
+    validateForMissingValue(password2, password2Error, passwordLabel, event);
 });
 
 firstName.addEventListener("input", () => {
     if (submitted) {
-        if (!firstName.value) {
-            firstNameError.innerHTML = `<i class="bi bi-patch-exclamation"></i> First name is required`;
-            firstName.classList.add("active-error");
-        } else {
-            firstNameError.textContent = "";
-            firstName.classList.remove("active-error");
-        }
+        validateForMissingValue(firstName, firstNameError, firstNameLabel);
     }
 });
 
 lastName.addEventListener("input", () => {
     if (submitted) {
-        if (!lastName.value) {
-            lastNameError.innerHTML = `<i class="bi bi-patch-exclamation"></i> First name is required`;
-            lastName.classList.add("active-error");
-        } else {
-            lastNameError.textContent = "";
-            lastName.classList.remove("active-error");
-        }
+        validateForMissingValue(lastName, lastNameError, lastNameLabel);
     }
 });
+
+email.addEventListener("input", () => {
+    if (submitted) {
+        validateForMissingValue(email, emailError, emailLabel);
+    }
+});
+
+phone.addEventListener("input", () => {
+    if (submitted) {
+        validateForMissingValue(phone, phoneError, phoneLabel);
+    }
+});
+
+password.addEventListener("input", () => {
+    if (submitted) {
+        validateForMissingValue(password, passwordError, passwordLabel);
+    }
+});
+
+password2.addEventListener("input", () => {
+    if (submitted) {
+        validateForMissingValue(password2, password2Error, passwordLabel);
+    }
+});
+
+function validateForMissingValue(element, elementError, elementLabel, event = null) {
+    const formattedElementName = capitalize(elementLabel.textContent);
+    if (!element.value) {
+        elementError.innerHTML = `<i class="bi bi-patch-exclamation"></i> ${formattedElementName} is required`;
+        element.classList.add("active-error");
+        if (event) {
+            event.preventDefault();
+        }
+    } else {
+        elementError.textContent = "";
+        element.classList.remove("active-error");
+    }
+}
+
+function capitalize(str) {
+    return str.charAt(1) + str.slice(2).toLowerCase();
+}
